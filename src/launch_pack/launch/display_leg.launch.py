15deabbba13cf5da774ca38015cce612b1bf2c0c
lyz1202dt <cloud1202@qq.com>
2025-12-15 17:34:36 +0800

    微调了URDF的关节限制，修正了关节初始角度

--- src/launch_pack/launch/display_leg.launch.py
+++ src/launch_pack/launch/display_leg.launch.py
@@ -19,8 +19,8 @@ def generate_launch_description():
     )
 
     joint_state_publish = Node(
-        package="joint_state_publisher",
-        executable="joint_state_publisher",
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
         parameters=[{
                 'use_gui': True
             }]
