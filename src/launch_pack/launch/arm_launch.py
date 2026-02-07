b96fec5764629ab42a98cd32b3b69add41f17be3
wzhzlh <2902981758@qq.com>
2026-02-07 13:27:29 +0800

    5

--- src/launch_pack/launch/arm_launch.py
+++ src/launch_pack/launch/arm_launch.py
@@ -1,17 +1,47 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
 import os
+
 def generate_launch_description():
-    
-    arm_pub = Node(
-        package="arm_pub",
-        executable="arm_pub"
+
+    urdf_path = os.path.join(
+        get_package_share_directory("dog"), 
+        "arm", "arm.urdf"  
     )
+    with open(urdf_path, 'r') as inf:
+        robot_desc = inf.read()
+
+    robot_state_pub = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_desc}]  
+    )
+
+   
+    arm_pub = Node(
+        package="leg_driver",
+        executable="arm_drive"
+    )
+    
     arm_calc = Node(
         package="leg_calc",
         executable="arm_calc"
     )
-    return  LaunchDescription([arm_pub, arm_calc])
+
+    # rviz2_config_path = os.path.join(
+    #     get_package_share_directory("launch_pack"),  
+    #     "rviz", "display_config.rviz"
+    # )
+
+    # rviz2 = Node(
+    #     package="rviz2",
+    #     executable="rviz2",
+    #     arguments=["-d", rviz2_config_path]  # 指定rviz配置文件
+    # )
+
+    return LaunchDescription([
+        robot_state_pub,  
+        arm_pub,         
+        arm_calc        
+    ])
